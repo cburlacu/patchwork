cf7977d1bc9169c970c85e60a014c05e3c9ffcd7
Damien Lespiau <damien.lespiau@intel.com>
2016-02-25 12:08:29 +0000

    admin: Add the project when displaying the list of tests
    It's useful to be able to see/filter Tests by project.
    
    Signed-off-by: Damien Lespiau <damien.lespiau@intel.com>

--- patchwork/admin.py
+++ patchwork/admin.py
@@ -104,7 +104,8 @@ admin.site.register(Comment, CommentAdmin)
 
 
 class TestAdmin(admin.ModelAdmin):
-    pass
+    list_display = ('name', 'project')
+    list_filter = ('project', )
 admin.site.register(Test, TestAdmin)
 
 
